24c572415fc93d316696ccf945cc655a1198d3fb
Dinesh Balasubramanian <dineshudt17@gmail.com>
2016-06-25 13:44:45 +0530

    Dinesh: Added tabs for interview and myinterviews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Routes, ROUTER_DIRECTIVES } from '@angular/router';
 import { Interviews } from '../interviews/interviews.component'
-
+import { TAB_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 @Component({
     selector: 'app',
     template: require('./app.component.html'),
-    directives: [ROUTER_DIRECTIVES],
+    directives: [ROUTER_DIRECTIVES, TAB_DIRECTIVES],
 })
 
 @Routes([
